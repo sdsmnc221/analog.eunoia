633ac62033b83e7caa2234cb84082b97eca7601c
sdsmnc221 <antr.2201@gmail.com>
2024-01-20 22:58:30 +0100

    test(app): add header to axios

--- src/helpers/axiosInstance.ts
+++ src/helpers/axiosInstance.ts
@@ -6,6 +6,11 @@ const axiosInstance = axios.create({
     Authorization: `Bearer ${import.meta.env.VITE_NOTION_API_KEY}`,
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   },
 });
 
