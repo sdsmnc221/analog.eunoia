756d181c8ccb6f35d1b2f366e62cf8f4f4bc67aa
sdsmnc221 <antr.2201@gmail.com>
2024-01-21 10:40:15 +0100

    feat(app): config chart and f5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <main>
     <Line id="mood-tracker-chart" :options="chartOptions" :data="chartData" />
+    <span class="refresh-button" @click="refreshData">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-refresh-ccw"
+      >
+        <path d="M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+        <path d="M3 3v5h5" />
+        <path d="M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16" />
+        <path d="M16 16h5v5" />
+      </svg>
+    </span>
   </main>
 </template>
 
@@ -9,7 +28,7 @@ import "the-new-css-reset/css/reset.css";
 
 import axiosInstance from "./helpers/axiosInstance";
 
-import { Ref, ref, watch } from "vue";
+import { Ref, onMounted, ref, watch } from "vue";
 
 import {
   Chart as ChartJS,
@@ -88,36 +107,57 @@ const chartOptions = {
       text: "Daily Tracker Viz",
     },
   },
+  animations: {
+    tension: {
+      duration: 1000,
+      easing: "linear",
+      from: 1,
+      to: 0.32,
+      loop: true,
+    },
+  },
+  scales: {
+    y: {
+      // defining min and max so hiding the dataset does not change scale range
+      min: 0,
+      max: 10,
+    },
+  },
 };
 
 const chartData: Ref<iChartData> = ref(computeChartData());
 
-axiosInstance
-  .post(
-    `${import.meta.env.DEV ? "" : "?query="}v1/databases/${
-      import.meta.env.VITE_NOTION_DATABASE_WIP
-    }/query`,
-    {
-      sorts: [
-        {
-          property: "Date",
-          direction: "ascending",
-        },
-      ],
-    }
-  )
-  .then((res) => {
-    if (res && res.data) {
-      const data_ = import.meta.env.DEV
-        ? res.data.results
-        : res.data.data.results;
-      tracker.value = data_.slice(-30).map((dataByDay: any) => ({
-        date: dataByDay.properties.Date.date.start,
-        mood: dataByDay.properties.Mood.number ?? 0,
-        habit: dataByDay.properties["Habit Progress"].formula.number * 10 ?? 0,
-      }));
-    }
-  });
+const refreshData = () => {
+  tracker.value = [];
+
+  axiosInstance
+    .post(
+      `${import.meta.env.DEV ? "" : "?query="}v1/databases/${
+        import.meta.env.VITE_NOTION_DATABASE_WIP
+      }/query`,
+      {
+        sorts: [
+          {
+            property: "Date",
+            direction: "ascending",
+          },
+        ],
+      }
+    )
+    .then((res) => {
+      if (res && res.data) {
+        const data_ = import.meta.env.DEV
+          ? res.data.results
+          : res.data.data.results;
+        tracker.value = data_.slice(-30).map((dataByDay: any) => ({
+          date: dataByDay.properties.Date.date.start,
+          mood: dataByDay.properties.Mood.number ?? 0,
+          habit:
+            dataByDay.properties["Habit Progress"].formula.number * 10 ?? 0,
+        }));
+      }
+    });
+};
 
 watch(
   () => tracker.value,
@@ -127,4 +167,28 @@ watch(
 );
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.refresh-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #edf6f9;
+  position: fixed;
+  top: 16px;
+  right: 16px;
+  cursor: pointer;
+  padding: 8px;
+  border-radius: 8px;
+  transition: transform 0.64s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  svg {
+    * {
+      stroke: #006d77;
+    }
+  }
+}
+</style>
