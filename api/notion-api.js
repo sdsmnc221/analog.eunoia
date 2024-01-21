fcd4bf2520f72cdf2d7847ba77e51b244b19181e
sdsmnc221 <antr.2201@gmail.com>
2024-01-21 01:03:54 +0100

    fix(app): notion-api

--- api/notion-api.js
+++ api/notion-api.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const axios = require("axios");
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -11,13 +14,15 @@ app.all("/notion-api/*", async (req, res) => {
     const path = req.params[0];
     const url = `${apiEndpoint}/${path}`;
 
+    console.log("Constructed URL:", url);
+
     const response = await axios({
       method: req.method,
       url,
       headers: {
         "Content-Type": "application/json",
         "Notion-Version": "2022-06-28",
-        Authorization: `Bearer ${import.meta.env.VITE_NOTION_API_KEY}`,
+        Authorization: `Bearer ${process.env.VITE_NOTION_API_KEY}`,
       },
       data: req.method !== "GET" ? req.body : undefined,
     });
@@ -33,4 +38,8 @@ app.all("/notion-api/*", async (req, res) => {
   }
 });
 
-module.exports = app;
+// app.listen("3000", () => {
+//   console.log(`Server is running on port 3000`);
+// });
+
+export default app;
