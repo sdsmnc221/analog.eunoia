7f183c01ff96baa5b2867b9a0b38d7d1331ec028
sdsmnc221 <antr.2201@gmail.com>
2024-01-21 01:08:22 +0100

    fix(app): notion-api svl fn

--- api/notion-api.js
+++ api/notion-api.js
@@ -1,45 +1,28 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
+// api/notion-api.js
+require("dotenv").config();
+const axios = require("axios");
 
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
-
-app.all("/notion-api/*", async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const apiEndpoint = "https://api.notion.com";
-    const path = req.params[0];
+    const path = req.url.replace(/^\/notion-api\//, "");
     const url = `${apiEndpoint}/${path}`;
-
-    console.log("Constructed URL:", url);
 
     const response = await axios({
       method: req.method,
       url,
       headers: {
         "Content-Type": "application/json",
-        "Notion-Version": "2022-06-28",
         Authorization: `Bearer ${process.env.VITE_NOTION_API_KEY}`,
       },
       data: req.method !== "GET" ? req.body : undefined,
     });
 
     // Set the response headers to match the original response
-    res.set(response.headers);
-
-    // Forward the status code and data from the Notion API response
+    res.setHeader("Content-Type", "application/json");
     res.status(response.status).json(response.data);
   } catch (error) {
     console.error("Error:", error);
     res.status(502).json({ error: "Bad Gateway" });
   }
-});
-
-// app.listen("3000", () => {
-//   console.log(`Server is running on port 3000`);
-// });
-
-export default app;
+};
